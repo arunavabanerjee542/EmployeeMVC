851d97cb5a41035dcf56d36843a0474eb2deeb32
Arunava Banerjee <Arunava Banerjee@DESKTOP-RBAIMPE>
2021-02-08 06:37:07 -0800

    Add project files.

---
+++ EmployeeManagementMVC/Controllers/EmployeeController.cs
@@ -0,0 +1,56 @@
+﻿using EmployeeManagementMVC.Models;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace EmployeeManagementMVC.Controllers
+{
+    public class EmployeeController : Controller
+    {
+       // private EmployeeService employeeService;
+
+        
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult ShowDetails()
+        {
+             IEnumerable<Employee> empList = EmployeeService.GetEmployeeList();
+
+            return View(empList);
+
+        }
+
+        [HttpGet]
+        public ActionResult AddEmployee()
+        {
+            return View();
+        }
+
+
+        [HttpPost]
+        public IActionResult AddEmployee(Employee employee)
+        {
+            Console.WriteLine(employee.name + " "+employee.name +  " " +employee.Department);
+            EmployeeService.Add(employee);
+            return RedirectToAction("ShowDetails");
+           // return Content ( employee.id + " " + employee.name + " " + employee.Department);
+
+
+        }
+
+
+
+
+
+
+
+
+
+
+    }
+}
---
+++ EmployeeManagementMVC/Models/EmployeeService.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace EmployeeManagementMVC.Models
+{
+    public static class EmployeeService
+    {
+        private static List<Employee> _employeeList = new List<Employee>();
+
+        /*
+        public EmployeeService()
+        {
+            _employeeList = new List<Employee>()
+            {
+                new Employee(){id = 1,name="Ram",Department=Department.SoftwareDevelopement},
+                  new Employee(){id = 2,name="Rama",Department=Department.Sales}
+
+            };
+        }
+        */
+
+        public static void Add(Employee employee)
+        {
+            _employeeList.Add(employee);
+        }
+
+        public static List<Employee> GetEmployeeList()
+        {
+            return _employeeList;
+        }
+
+
+
+    }
+}
